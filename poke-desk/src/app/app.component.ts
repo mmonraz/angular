750b8ba03f3001dd0789110773c72eb60a864a3e
Miguel Monraz <miguel_monraz@epam.com>
2024-01-18 14:43:23 -0700

    Finish task 1 without CSS_

--- poke-desk/src/app/app.component.ts
+++ poke-desk/src/app/app.component.ts
@@ -7,4 +7,36 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'poke-desk';
+  pokemons = [
+    {
+      "name": "bulbasaur",
+      "id": 1,
+      "forms_switchable": false
+    },
+    {
+      "name": "ivysaur",
+      "id": 2,
+      "forms_switchable": false
+    },
+    {
+      "name": "venusaur",
+      "id": 3,
+      "forms_switchable": true
+    },
+    {
+      "name": "charmander",
+      "id": 4,
+      "forms_switchable": false
+    },
+    {
+      "name": "charmeleon",
+      "id": 5,
+      "forms_switchable": false
+    },
+    {
+      "name": "charizard",
+      "id": 6,
+      "forms_switchable": true
+    }
+];
 }
--- poke-desk/src/app/poke-card/poke-card.component.ts
+++ poke-desk/src/app/poke-card/poke-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-poke-card',
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrl: './poke-card.component.css'
 })
 export class PokeCardComponent {
-
+  @Input() pokemons;
 }
