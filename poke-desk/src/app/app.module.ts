230b060689ed80482afb8a58bca6985f20b8a52b
Miguel Monraz <miguel_monraz@epam.com>
2024-01-18 14:27:09 -0700

    First changes

---
+++ poke-desk/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PokeCardComponent } from './poke-card/poke-card.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PokeCardComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ poke-desk/src/app/poke-card/poke-card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-poke-card',
+  templateUrl: './poke-card.component.html',
+  styleUrl: './poke-card.component.css'
+})
+export class PokeCardComponent {
+
+}
